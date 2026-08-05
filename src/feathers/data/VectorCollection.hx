4b678aaed56ebf67edbaff8968cd8d8f1227c49d
Joseph Cloutier <player3.14@gmail.com>
2026-08-05 14:52:47 -0400

    What is going on?

--- src/feathers/data/VectorCollection.hx
+++ src/feathers/data/VectorCollection.hx
@@ -580,11 +580,12 @@ class VectorCollection<T> extends EventDispatcher implements IFlatCollection<T> 
 			var newLength:Int = 0;
 			for (item in this._filterAndSortData) {
 				if (this._filterFunction(item)) {
+					#if (mac && hl)
 					if (newLength >= this._filterAndSortData.length) {
-						this._filterAndSortData.push(item);
-					} else {
-						this._filterAndSortData[newLength] = item;
+						throw new RangeError('Expected newLength < _filterAndSortData.length, but got $newLength >= ${this._filterAndSortData.length}, indicating that _filterAndSortData is somehow longer than itself.');
 					}
+					#end
+					this._filterAndSortData[newLength] = item;
 					newLength++;
 				}
 			}
